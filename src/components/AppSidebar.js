93f4c29e0bf03fd8433e9ed7a9b89fc6427bee7b
Jegan <jeganvishwa@yahoo.com>
2025-09-12 15:04:31 +0530

    sidebar

--- src/components/AppSidebar.js
+++ src/components/AppSidebar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
 import {
@@ -10,18 +10,49 @@ import {
   CSidebarToggler,
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
-
 import { AppSidebarNav } from './AppSidebarNav'
-
-// sidebar nav config
-import navigation from '../_nav'
+import * as icons from '@coreui/icons'
+import { CNavGroup, CNavItem, CNavTitle } from '@coreui/react'
+const BASE = import.meta.env.VITE_BASE_URL;
 
 const AppSidebar = () => {
   const dispatch = useDispatch()
   const unfoldable = useSelector((state) => state.sidebarUnfoldable)
   const sidebarShow = useSelector((state) => state.sidebarShow)
-  
+  const [navigation, setNavigation] = useState([])
 
+  useEffect(() => {
+   const authToken = JSON.parse(sessionStorage.getItem('authToken')) || '';
+    fetch(`${BASE}menu/menus`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${authToken}`,
+        }
+      })
+      .then((res) => res.json())
+      .then((data) => {
+        const parsed = data.map((item) => transformItem(item))
+        setNavigation(parsed)
+      })
+      .catch((err) => console.error(err))
+  }, [])
+
+  const transformItem = (item) => {
+    if (item.icon) {
+      item.icon = <CIcon icon={icons[item.icon.icon]} customClassName={item.icon.customClassName} />
+    }
+
+    if (item.items) {
+      item.items = item.items.map((sub) => transformItem(sub))
+    }
+
+    if (item.component === 'CNavItem') item.component = CNavItem
+    if (item.component === 'CNavGroup') item.component = CNavGroup
+    if (item.component === 'CNavTitle') item.component = CNavTitle
+
+    return item
+  }
 
   return (
     <CSidebar
@@ -31,14 +62,12 @@ const AppSidebar = () => {
       unfoldable={unfoldable}
       visible={sidebarShow}
       onVisibleChange={(visible) => {
-        dispatch({ type: 'set', sidebarShow: visible });
+        dispatch({ type: 'set', sidebarShow: visible })
       }}
     >
       <CSidebarHeader className="border-bottom">
         <CSidebarBrand to="/">
-           {/* <img customClassName="sidebar-brand-full" src={unfoldable==true ? '/mini.webp' : '/logo.webp'} height={32} /> */}
-           <h1>AMS APP</h1>
-           
+          <h1>AMS APP</h1>
         </CSidebarBrand>
         <CCloseButton
           className="d-lg-none"
@@ -46,9 +75,10 @@ const AppSidebar = () => {
           onClick={() => dispatch({ type: 'set', sidebarShow: false })}
         />
       </CSidebarHeader>
+
       <AppSidebarNav items={navigation} />
+
       <CSidebarFooter className="border-top d-none d-lg-flex">
-      {unfoldable==true ? '' : ''}
         <CSidebarToggler
           onClick={() => dispatch({ type: 'set', sidebarUnfoldable: !unfoldable })}
         />
